d9dfdf61071c66858cc6a1b65e7b6e693c6786ad
Andrew <andslobby@gmail.com>
2021-09-22 10:40:16 +0300

    feat: make task4.10

--- ShcherbaAndrew/task_4_10.py
+++ ShcherbaAndrew/task_4_10.py
@@ -1,40 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from typing import Set
+from typing import Dict
 from functools import reduce
 import string
 
 
-def test_1_1(*strings: str) -> Set[str]:
-    return reduce(lambda acc, s: acc & s,
-                  map(lambda x: set(x), strings))
-
-def test_1_2(*strings: str) -> Set[str]:
-    return reduce(lambda acc, s: acc | s,
-                  map(lambda x: set(x), strings))
-
-def test_1_3(*strings: str) -> Set[str]:
-    acc = set()
-    list_set = list(map(lambda x: set(x), strings))
-    for _ in range(len(list_set)-1):
-        s = list_set.pop()
-        for item in list_set:
-            acc = acc | s & item
-    return acc
-
-def test_1_4(*strings: str) -> Set[str]:
-    return reduce(lambda acc, s: acc - s,
-                  map(lambda x: set(x.lower()), strings),
-                  set(string.ascii_lowercase),
-                  )
+def generate_squares(num: int) -> Dict[int, int]:
+    return {i: i**2 for i in range(1, num+1)}
 
 def main():
-    test_strings = ["hello", "world", "python", ]
-    print(test_1_1(*test_strings))
-    print(test_1_2(*test_strings))
-    print(test_1_3(*test_strings))
-    print(test_1_4(*test_strings))
+    print(generate_squares(5))
+
 
 if __name__ == "__main__":
     main()
