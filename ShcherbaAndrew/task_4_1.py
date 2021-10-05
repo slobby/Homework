f3f528b1aa769919f292112fc8bc03d1024fbefc
Andrew <andslobby@gmail.com>
2021-10-05 12:25:26 +0300

    feat: make Task4.1

---
+++ ShcherbaAndrew/task_4_1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import itertools
+import pprint
+
+
+class OutOfRangeError(Exception):
+    def __str__(self):
+        return "Maximal value is reached"
+
+
+class Counter:
+    def __init__(self, start=0, stop=None) -> None:
+        self._start = start
+        self._stop = stop
+
+    def increment(self):
+        try:
+            if self._stop and self._start == self._stop:
+                raise OutOfRangeError
+            self._start += 1
+        except OutOfRangeError as ex:
+            print(ex)
+
+    def get(self):
+        print(self._start)
+
+
+if __name__ == "__main__":
+    c = Counter(start=42)
+    c.increment()
+    c.get()
+
+    c = Counter()
+    c.increment()
+    c.get()
+    c.increment()
+    c.get()
+
+    c = Counter(start=42, stop=43)
+    c.increment()
+    c.get()
+    c.increment()
+    c.get()
