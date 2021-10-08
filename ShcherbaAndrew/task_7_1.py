5710313b9ce916140888a94aed58f7b6a343c7d5
Andrew <andslobby@gmail.com>
2021-10-08 22:43:49 +0300

    feat: make task_7_1

---
+++ ShcherbaAndrew/task_7_1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class FileManager:
+    def __init__(self, filename, mode="r", encoding="utf-8"):
+        self._filename = filename
+        self._mode = mode
+        self._encoding = encoding
+        self._file = None
+
+    def __enter__(self):
+        self._file = open(self._filename, self._mode, encoding=self._encoding)
+        return self._file
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if self._file is not None:
+            self._file.close()
+        if exc_type:
+            print(exc_value)
+
+
+if __name__ == "__main__":
+    fm = FileManager("test.csv")
+    with fm as f:
+        print(f.readline())
+        f.write("Blabla")
